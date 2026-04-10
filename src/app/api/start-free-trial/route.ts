6ac4f7212c849631c59a2a24051b6b415f56d02d
dolatjangir <dolatjangir778@gmail.com>
2026-04-10 11:04:17 +0530

    add-requirement-page-or-explpore-dash-explore-ui2

--- src/app/api/start-free-trial/route.ts
+++
@@ -1,8 +0,0 @@
-// import { PrismaClient } from "@prisma/client";
-// import { NextRequest } from "next/server";
-
-// export async function POST(req:Request){
-// try{
-//     const {email,first}
-// }
-// }
